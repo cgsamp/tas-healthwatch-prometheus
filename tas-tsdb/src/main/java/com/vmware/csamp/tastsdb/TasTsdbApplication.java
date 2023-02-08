2c3b3493f3892c22ecfa81a93ba14db20036bd29
Christopher Samp <chrisgsamp@gmail.com>
2023-02-08 14:03:15 -0500

    Added Spring Boot template project

---
+++ tas-tsdb/src/main/java/com/vmware/csamp/tastsdb/TasTsdbApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.csamp.tastsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasTsdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasTsdbApplication.class, args);
+	}
+
+}
---
+++ tas-tsdb/src/test/java/com/vmware/csamp/tastsdb/TasTsdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.csamp.tastsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasTsdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
